3e18131228799284e8f7496fcbead8afbba97a89
Vittorio Adesso <adessovittorio8@gmail.com>
2022-09-03 16:51:24 +0200

    loop

---
+++ index_2.js
@@ -0,0 +1,21 @@
+function fizzBuzz() {
+    for ( let i = 1; i <= 100 ; i ++)
+    // PPONEMOS DE PRIMERO PORQUE SINO COMPROMENTE LOS SIGUIENTES RECORRIDOS .... 
+    if ( i % 15 === 0 ) { //15 es múltiplo de 3 y 5 ... 
+        console.log('fizzBuzz');
+    } else if( i % 3 === 0) { // múltiplos de 3, en división dan 0
+        console.log('fizz'); 
+        } else if ( i % 7 === 0 ) { // múltiplos de 5
+            console.log('buzz');
+        }  
+        else if ( i % 7 === 0 ) { // múltiplos de 7
+            console.log('WOF');
+        }  
+
+        else{
+            console.log(i)
+        }
+    
+}
+
+fizzBuzz(); 
---
+++ readme.md
@@ -0,0 +1,5 @@
+## fizzBuzz
+
+Escribe un programa que muetsra los números del 1 al 100 sustityutendo los múltiplos de 3 por la palabra "fizz", los múltiplos de 5 por la palabra "buzz" y los múltiplos de ambos ( 3 y 5) por la palabra "fizzbuzz"...
+
+& extra with multiplo de 7 ....🔎💡
